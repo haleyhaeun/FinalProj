8bb944e647fa482febc79267bdfe125adf1d1c60
Haley <hnohhe2022@gmail.com>
2022-07-20 09:29:37 -0700

    Initial Commit

---
+++ FinalProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProj
+//
+//  Created by Haley Noh on 2022/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
